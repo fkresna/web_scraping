c21dd587a5f081499804ddb4878fd5ecdff3d0d1
Freddy Tjoenedi <fkresna@gmail.com>
2016-04-26 01:12:00 -0400

    Data transformation postal code

--- transformPostalCode.py
+++ transformPostalCode.py
@@ -1,4 +1,5 @@
 import csv
+import zipcode
 from re import sub
 from decimal import Decimal
 
@@ -12,15 +13,24 @@ finally:
 
 with open('cleanData2.csv','rb') as csvfile:
     reader = csv.DictReader(csvfile)
-    count = 0
     count2 = 0
     for row in reader:
     	f = open(cleanCSVFile, 'a')
-    	try:
-		writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-	writer.writerow((row['Boat Year'],row['Boat Model'],row['Boat Make'],row['Seller Name'],row['Postal Code'],row['Boat Price'],row['Boat Class'],row['Boat Category'],row['Boat Length'],row['Propolsion Type'],row['Hull Material'],row['Fuel Type']))
-	count2 = count2 + 1
-            finally:
-                f.close()
-        count = 0
+        if row['Postal Code'] != '0':
+            try:
+                if len(row['Postal Code']) > 5:
+                    temp = row['Postal Code']
+                    temp = temp[:5]
+                    myzip = zipcode.isequal(temp)
+                else:
+                    myzip = zipcode.isequal(row['Postal Code'])
+                if myzip != None:
+                    try:
+                        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+                        writer.writerow((row['Boat Year'],row['Boat Model'],row['Boat Make'],row['Seller Name'],row['Postal Code'],row['Boat Price'],row['Boat Class'],row['Boat Category'],row['Boat Length'],row['Propolsion Type'],row['Hull Material'],row['Fuel Type'],myzip.city,myzip.state))
+                        count2 = count2 + 1
+                    finally:
+                        f.close()
+            except:
+                print row['Postal Code']
     print count2
