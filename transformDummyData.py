5e2a895f9f49a39edd70597618b10517828b45ef
Freddy Tjoenedi <fkresna@gmail.com>
2016-04-28 20:03:19 -0400

    add dummy for fuel type

--- transformDummyData.py
+++ transformDummyData.py
@@ -7,9 +7,9 @@ cleanCSVFile = 'dummyData.csv'
 f = open(cleanCSVFile,'wt')
 try:
     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow( ('Price', 'C.Pwc','C.Power','C.Sails','C.Small'));
+    writer.writerow( ('Price', 'C.Pwc','C.Power','C.Sails','C.Small','F.Diesel','F.Electric','F.Gas','F.Other','F.Propane'));
 
-#,'P.Other','P.SingleIn','P.SingleOut','P.TripleOut','P.TwinIn','P.TwinOut','H.Aluminium','H.Composite','H.Ferro','H.Fiberglass','H.Hypalon','H.Other','H.Pvc','H.Roplene','H.Steel','H.Wood','F.Diesel','F.Electric','F.Gas','F.Other','F.Propane','State') )
+#,'P.Other','P.SingleIn','P.SingleOut','P.TripleOut','P.TwinIn','P.TwinOut','H.Aluminium','H.Composite','H.Ferro','H.Fiberglass','H.Hypalon','H.Other','H.Pvc','H.Roplene','H.Steel','H.Wood','State') )
 
 finally:
     f.close()
@@ -40,9 +40,40 @@ with open('cleanData3.csv','rb') as csvfile:
             sails = 0
             small = 1
 
+        if row['Fuel Type'] == 'Diesel':
+            diesel = 1
+            electric = 0
+            gas = 0
+            other = 0
+            propane = 0
+        elif row['Fuel Type'] == 'Electric':
+            diesel = 0
+            electric = 1
+            gas = 0
+            other = 0
+            propane = 0
+        elif row['Fuel Type'] == 'Gas':
+            diesel = 0
+            electric = 0
+            gas = 1
+            other = 0
+            propane = 0
+        elif row['Fuel Type'] == 'Other':
+            diesel = 0
+            electric = 0
+            gas = 0
+            other = 1
+            propane = 0
+        elif row['Fuel Type'] == 'Propane':
+            diesel = 0
+            electric = 0
+            gas = 0
+            other = 0
+            propane = 1
+
         try:
             writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-            writer.writerow((row['Boat Price'],pwc,power,sails,small))
+            writer.writerow((row['Boat Price'],pwc,power,sails,small,diesel,electric,gas,other,propane))
             count2 = count2 + 1
         finally:
             f.close()
