44faee1841af41df9b9030a4a4699cf69a37f42b
Freddy Tjoenedi <fkresna@gmail.com>
2016-04-29 21:58:56 -0400

    add east coast

--- transformDummyData.py
+++ transformDummyData.py
@@ -7,9 +7,8 @@ cleanCSVFile = 'dummyData.csv'
 f = open(cleanCSVFile,'wt')
 try:
     writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow( ('Price', 'C.Pwc','C.Power','C.Sails','C.Small','F.Diesel','F.Electric','F.Gas','F.Other','F.Propane','P.Other','P.SingleIn','P.SingleOut','P.TripleOut','P.TwinIn','P.TwinOut','H.Aluminium','H.Composite','H.Ferro','H.Fiberglass','H.Hypalon','H.Other','H.Pvc','H.Roplene','H.Steel','H.Wood'));
+    writer.writerow( ('Price', 'C.Pwc','C.Power','C.Sails','C.Small','F.Diesel','F.Electric','F.Gas','F.Other','F.Propane','P.Other','P.SingleIn','P.SingleOut','P.TripleOut','P.TwinIn','P.TwinOut','H.Aluminium','H.Composite','H.Ferro','H.Fiberglass','H.Hypalon','H.Other','H.Pvc','H.Roplene','H.Steel','H.Wood', 'eastCoast'));
 
-#'State'
 
 finally:
     f.close()
@@ -19,6 +18,11 @@ with open('cleanData3.csv','rb') as csvfile:
     count2 = 0
     for row in reader:
     	f = open(cleanCSVFile, 'a')
+        
+        eastCoast = 0
+        if row['State'] == 'FL' or row['State'] == 'GA' or row['State'] == 'SC' or row['State'] == 'NC' or row['State'] == 'VA' or row['State'] == 'MD' or row['State'] == 'DE' or row['State'] == 'NJ' or row['State'] == 'NY' or row['State'] == 'CT' or row['State'] == 'RI' or row['State'] == 'MA' or row['State'] == 'NH' or row['State'] == 'ME':
+            eastCoast = 1
+
         hAluminium = 0
         hComposite = 0
         hFerro = 0
@@ -100,7 +104,7 @@ with open('cleanData3.csv','rb') as csvfile:
 
         try:
             writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-            writer.writerow((row['Boat Price'],pwc,power,sails,small,diesel,electric,gas,other,propane,pOther,pSingleIn,pSingleOut,pTripleOut,pTwinIn,pTwinOut,hAluminium, hComposite, hFerro, hFiberglass, hHypalon, hOther, hPvc, hRoplene, hSteel, hWood ))
+            writer.writerow((row['Boat Price'],pwc,power,sails,small,diesel,electric,gas,other,propane,pOther,pSingleIn,pSingleOut,pTripleOut,pTwinIn,pTwinOut,hAluminium, hComposite, hFerro, hFiberglass, hHypalon, hOther, hPvc, hRoplene, hSteel, hWood,eastCoast ))
             count2 = count2 + 1
         finally:
             f.close()
